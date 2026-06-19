d05f16f29869ac2b162eab9057550bcc6cf3ce91
Carlos Vel <carlosxt10321710@gmail.com>
2026-06-19 13:32:29 -0600

    bd?

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,9 @@
 python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@correo.com', 'admin1234')
+"
